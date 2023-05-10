43d6d3ab5abe87f1b5a77c1c400e7a99090bcb07
dr3nan <dolgarrenan@gmail.com>
2023-05-10 16:24:46 +0200

    feat: initial function to get user by ID (WIP), refs #100

--- client/src/App/services/api.service.ts
+++ client/src/App/services/api.service.ts
@@ -1,1 +1,17 @@
 const CLFRAMEWORK_URL = process.env.REACT_APP_CLFRAMEWORK_URL;
+
+export const getUserByID = async (userId: number) => {
+  try {
+    const url = `${CLFRAMEWORK_URL}/users/${userId}`;
+    const response = await fetch(url, {
+      method: 'GET',
+      headers: {
+        'X-WEB-KEY': 'Development',
+        'Content-Type': 'application/json'
+      }
+    });
+    return response.json()
+  } catch (err) {
+    console.error(err)
+  }
+};
