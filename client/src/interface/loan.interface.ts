ea74e80294e2e9f9e8b223015d2765fe304ab9ff
dr3nan <dolgarrenan@gmail.com>
2023-05-10 12:12:44 +0200

    feat: interfaces user and loan created, refs #100

---
+++ client/src/interface/loan.interface.ts
@@ -0,0 +1,7 @@
+interface ILoanData {
+  loan_amount: number;
+  loan_date: string;
+  loan_weeks: number;
+}
+
+export default ILoanData;
---
+++ client/src/interface/user.interface.ts
@@ -0,0 +1,14 @@
+interface IUserData {
+  id: number;
+  name: string;
+  surname: string;
+  email: string;
+  phone: string;
+  age: {
+    day: number;
+    month: number;
+    year: number
+  };
+}
+
+export default IUserData;
