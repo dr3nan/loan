0c44d8f72172b3b48cb1c84d55bfb2886e22e24c
dr3nan <dolgarrenan@gmail.com>
2023-05-11 10:15:43 +0200

    refactor: chnage api import to a common variable (WIP), refs #100

--- client/src/App/services/api.service.ts
+++ client/src/App/services/api.service.ts
@@ -1,35 +1,29 @@
-// const CLFRAMEWORK = process.env.REACT_APP_CLFRAMEWORK_URL;
-// console.log(CLFRAMEWORK);
-
 import ILoanData from '../interface/loan.interface';
 import IUserData from '../interface/user.interface';
 
-export const getUserByID = async (userId: number) => {
-  // if (CLFRAMEWORK) {
-    try {
-      // const url = new URL(CLFRAMEWORK);
-      const url = 'https://api7.cloudframework.io/recruitment/fullstack/users?id=1';
-      // url.searchParams.append('id', userId.toString())
-      // const response = await fetch(url.toString(), {
-      const response = await fetch(url, {
-        method: 'GET',
-        headers: {
-          'X-WEB-KEY': 'Development'
-        }
-      });
+const BASE_URL = 'https://api7.cloudframework.io/recruitment/fullstack/users?id=';
 
-      return await response.json();
-    } catch (err) {
-      console.error(err)
-    }
-  // }
+export const getUserByID = async (userId: number) => {
+  try {
+    const url = `${BASE_URL}${userId}`;
+    const response = await fetch(url, {
+      method: 'GET',
+      headers: {
+        'X-WEB-KEY': 'Development'
+      }
+    });
+
+    return await response.json();
+  } catch (err) {
+    console.error(err)
+  }
 };
 
 export const updateUser = async (user: IUserData, loan: ILoanData) => {
   console.log('user', user);
   console.log('loan', loan);
   try {
-    const url = 'https://api7.cloudframework.io/recruitment/fullstack/users?id=1';
+    const url = `${BASE_URL}${user.id}`;
     const response = await fetch(url, {
       method: 'POST',
       headers: {
