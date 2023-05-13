1a5601b25ef856d39859a0cb64a0900949291e52
dr3nan <dolgarrenan@gmail.com>
2023-05-13 11:38:47 +0200

    feat: split form code in two, import props, searching best modularity, refs #111

--- client/src/App/components/Loan/Loan.tsx
+++ client/src/App/components/Loan/Loan.tsx
@@ -1,10 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import IUserLoanData from '../../interface/userLoan.interface';
 import { getUserByID, updateUser } from '../../services/api.service';
-import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 import './loan.scss';
 import './response.scss';
+import { UserForm } from '../UserForm/UserForm';
 
 export const Loan = () => {
   const [userLoanData, setUserLoanData] = useState<IUserLoanData>({
@@ -112,137 +112,12 @@ export const Loan = () => {
           <button onClick={() => setPostError('')}>Volver</button>
         </div>
       ) : (
-        <form className='user-details-form'
-          onSubmit={handleSubmit}
-        >
-          <label htmlFor='name'>
-            Nombre
-          </label>
-          <input
-            type='text'
-            name='name'
-            id='name'
-            value={userLoanData.name}
-            className='name'
-            readOnly
-          />
-          <label htmlFor='surname'>
-            Apellido
-          </label>
-          <input
-            type='text'
-            name='surname'
-            id='surname'
-            value={userLoanData.surname}
-            className='surname'
-            readOnly
-          />
-          <label htmlFor='email'>
-            Email
-          </label>
-          <input
-            type='email'
-            name='email'
-            id='email'
-            value={userLoanData.email}
-            className='email'
-            readOnly
-          />
-          <label htmlFor='phone'>
-            Teléfono
-          </label>
-          <input
-            type='tel'
-            name='phone'
-            id='phone'
-            value={userLoanData.phone}
-            onChange={(event) => setUserLoanData({ ...userLoanData, phone: event.target.value })}
-            className='phone'
-            required
-          />
-          <label htmlFor='age'>
-            Edad
-          </label>
-          <input
-            type='number'
-            name='age'
-            id='age'
-            min={18}
-            max={120}
-            value={userLoanData.age}
-            onChange={(event) => setUserLoanData({ ...userLoanData, age: parseFloat(event.target.value) })}
-            className='age'
-            required
-          />
-          <label htmlFor='loan_amount'>
-            Importe del préstamo
-          </label>
-          <input
-            type='number'
-            min={11}
-            max={1000}
-            // step={1}
-            name='loan_amount'
-            id='loan_amount'
-            value={userLoanData.loan_amount}
-            onChange={(event) => setUserLoanData({ ...userLoanData, loan_amount: parseFloat(event.target.value) })}
-            className='loan_amount'
-            required
-          />
-          <label htmlFor='loan_date'>
-            Fecha a conseguir el préstamo
-          </label>
-          <DatePicker
-            selected={userLoanData.loan_date}
-            minDate={new Date()}
-            onChange={handleLoanDateChange}
-            dateFormat='yyy-MM-dd'
-            className='loan_date'
-            required
-          />
-          <label htmlFor='loan_weeks'>
-            Tiempo a devolver (en semanas)
-          </label>
-          <input
-            type='number'
-            min={1}
-            max={20}
-            step={1}
-            name='loan_weeks'
-            id='loan_weeks'
-            value={userLoanData.loan_weeks}
-            onChange={(event) => setUserLoanData({ ...userLoanData, loan_weeks: parseFloat(event.target.value) })}
-            className='loan_weeks'
-            required
-          />
-          <div className='checkbox'>
-            <label htmlFor='checkbox-label'>
-              <input
-                type='checkbox'
-                name='check'
-                id='check'
-                checked={userLoanData.check}
-                onChange={(event) => setUserLoanData({ ...userLoanData, check: event.target.checked })}
-                className='checkbox-input'
-                required
-              />
-              Aceptar{' '}
-              <a
-                href='https://cloudframework.io/terminos-y-condiciones/'
-                target='_blank'
-                rel='noreferrer'
-              >
-                Términos y Condiciones
-              </a>
-            </label>
-          </div>
-          <button
-            type='submit'
-            className='submit-button'
-          >
-            Enviar Solicitud
-          </button>
-        </form>
+        <UserForm
+          handleSubmit={handleSubmit}
+          userLoanData={userLoanData}
+          setUserLoanData={setUserLoanData}
+          handleLoanDateChange={handleLoanDateChange}
+        />
       )}
     </main>
   )
